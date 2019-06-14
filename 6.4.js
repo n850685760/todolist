fcea68935f845968f72beed3393f95adf93453b8
n850685760 <850685760@qq.com>
2019-06-14 08:38:08 +0800

    todolist

--- 6.4.js
+++ 6.4.js
@@ -1,7 +1,7 @@
 window.onload = function () {
-    let daohang = document.querySelectorAll('.daohang>li')
-    let content = document.querySelector('.content')
-    let type ='all'
+    // let daohang = document.querySelectorAll('.daohang>li')
+    let daohang = $('.daohang > li')
+    let content =document.querySelector('.content')
     let arr = [
         {
             id: 1, content: '123456', ctime: '2019-01-11', status: true
@@ -13,6 +13,11 @@ window.onload = function () {
             id: 3, content: '123789', ctime: '2019-07-11', status: false
         }
     ]
+    let str = localStorage.getItem('arr')
+    if (!str){
+        localStorage.setItem('arr',JSON.stringify(arr))
+    }
+    arr = JSON.parse(str)
     //渲染函数
     function x(arr) {
         let html = ``
@@ -38,7 +43,7 @@ window.onload = function () {
     }
     //过滤3
     let arr1 =[];
-    function filter() {
+    function filter(type) {
         switch (type) {
             case'all':arr1 = arr
                 break
@@ -56,29 +61,39 @@ window.onload = function () {
         content.innerHTML = x(arr1);
     }
     //标题点击事件
-        for (let i = 0; i < daohang.length; i++) {
-            daohang[i].onclick = function () {
-                type = this.type
-                for (let i = 0; i < daohang.length; i++) {
-                    daohang[i].classList.remove('hot');
-                }
-                this.classList.add('hot');
-                filter()
-            }
-        }
-        daohang[0].onclick()
+    daohang.on('click',function () {
+        let type = $(this).attr('type')
+            console.log(1);
+        $(this).addClass('hot').siblings('li').removeClass('hot')
+        filter(type)
+    })
+    daohang.triggerHandler('click')
+    //     for (let i = 0; i < daohang.length; i++) {
+    //         daohang[i].onclick = function () {
+    //             type = this.type
+    //             for (let i = 0; i < daohang.length; i++) {
+    //                 daohang[i].classList.remove('hot');
+    //             }
+    //             this.classList.add('hot');
+    //             filter()
+    //         }
+    //     }
+    //     daohang[0].onclick()
     //单选框添加事件
     content.onclick = function (e) {
         let index = arr.findIndex(ele => ele.id == e.target.parentNode.id)
         if (e.target.nodeName == 'DIV'){
             arr.splice(index,1)
+            localStorage.setItem('arr',JSON.stringify(arr))
             filter()
         }
         if (e.target.nodeName == 'INPUT'){
             if (arr[index].status) {
                 arr[index].status = false
+                localStorage.setItem('arr',JSON.stringify(arr))
             }else {
                 arr[index].status = true
+                localStorage.setItem('arr',JSON.stringify(arr))
             }
             filter()
         }
@@ -99,6 +114,7 @@ window.onload = function () {
                 ar.ctime = input[1].value
                 ar.status= false;
                 arr.push(ar)
+                localStorage.setItem('arr',JSON.stringify(arr))
                 filter()
             }
 
